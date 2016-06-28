99f3ac5d99c2b449c4c513725100b977e31b22db
Michael Dippery <michael@monkey-robot.com>
2016-06-27 21:24:00 -0700

    Playground for this lesson

---
+++ Tuples.playground/Contents.swift
@@ -0,0 +1,23 @@
+func downloadImage(atURL url: String) -> (Bool, String) {
+    // TODO: Download image
+    // if no error, return true
+    // otherwise, get error string and return false
+    let error = "Image no longer exists"
+    return (false, error)
+}
+
+let result = downloadImage(atURL: "http://example.com/image.png")
+result.0
+result.1
+print("Success? \(result.0)")
+print("Message: \(result.1)")
+
+let person = (name: "Jim", age: 30)
+person.age
+person.name
+print("\(person.name)'s age is \(person.age)")
+
+var person2 = (name: "Jim", age: 30)
+print("\(person2.name)'s age is \(person2.age)")
+person2.age = 32
+print("\(person2.name)'s age is \(person2.age)")
